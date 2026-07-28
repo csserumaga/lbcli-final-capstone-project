1e3e7a50d89c91d5a8597fd1b383b22deff24c91
csserumaga <alltechissues@gmail.com>
2026-07-28 14:26:09 +0300

    added

--- submission/02.sh
+++ submission/02.sh
@@ -1,10 +1,3 @@
 # How many new outputs were created by block 243,825?
-<<<<<<< HEAD
-blockhash=$(bitcoin-cli -signet getblockhash 243825)
-
-inputs=$(bitcoin-cli -signet getblock "$blockhash" 2 | jq '[.tx[].vin[] | select(.txid != null)] | length')
-
-echo "$inputs"
-=======
 bitcoin-cli -signet getblockstats 243825 '["outs"]' | jq -r '.outs'
->>>>>>> babfef7 ( fixed 2nd file)
+
