2468fe56fb0d5005fc464094660a121f66652f81
csserumaga <alltechissues@gmail.com>
2026-07-28 14:35:07 +0300

    added 6th

--- submission/06.sh
+++ submission/06.sh
@@ -1,5 +1,5 @@
 # Only one tx in block 243,821 signals opt-in RBF. What is its txid?
 blockHash=$(bitcoin-cli -signet getblockhash 243821)
 
-rbfTx=$(bitcoin-cli -signet getblock "$blockHash" 2 | jq -r '.tx[] | select(any(.vin[]?; .sequence < 4294967294)) | .txid')
+rbfTx=$(bitcoin-cli -signet getblock "$blockHash" 2 | jq -r '.tx[] | select(any(.vin[]; .txid != null and .sequence < 4294967294)) | .txid')
 
