da966d65cc8bb288cf5bbe3e66336c01531397c5
csserumaga <alltechissues@gmail.com>
2026-07-28 14:39:21 +0300

    added 8th file

--- submission/08.sh
+++ submission/08.sh
@@ -1,2 +1,10 @@
 # what block height was this tx mined ?
-# 49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb
+txid="49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb"
+
+blockHash=$(bitcoin-cli -signet getrawtransaction "$txid" true | jq -r '.blockhash')
+
+height=$(bitcoin-cli -signet getblockheader "$blockHash" | jq -r '.height')
+
+echo "$height"
+
+
